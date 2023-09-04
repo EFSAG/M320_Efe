eb45bc3f625b25eb0c0d7f7f9ead7a8c799d6374
EFSAG <e.bora.saglam@gmail.com>
2023-09-04 09:58:05 +0200

    Efe

---
+++ rectangle/rectangle/rectangle/Program.cs
@@ -0,0 +1,19 @@
+﻿using System.Drawing;
+namespace rectangle
+
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            rectangle rectangle = new rectangle (1, 2, Color.Blue, true);
+            rectangle.WriteProperties();
+            rectangle rectangle1 = new rectangle(5, 3, Color.Yellow, false);
+            rectangle1.WriteProperties();
+            rectangle rectangle2 = new rectangle(900, 22, Color.Blue, true);
+            rectangle2.WriteProperties();
+            rectangle rectangle3 = new rectangle(111, 234, Color.Black);
+            rectangle3.WriteProperties();
+        }
+    }
+}
---
+++ rectangle/rectangle/rectangle/rectangle.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace rectangle
+{
+    internal class rectangle
+    {
+        private int width {  get; set; }
+        private int height { get; set; }
+        private Color color { get; set; }
+        private bool isTransparent { get; set; }
+
+
+        public rectangle(int width)
+        {
+            this.width = width;
+        }
+
+        public rectangle(int width, int height)
+        {
+            this.width = width;
+            this.height = height;
+        }
+
+        public rectangle(int width, int height, Color color)
+        {
+            this.width = width;
+            this.height = height;
+            this.color = color;
+        }
+
+        public rectangle(int width, int height, Color color, bool isTransparent)
+        {
+            this.width = width;
+            this.height = height;
+            this.color = color;
+            this.isTransparent=isTransparent;
+        }
+
+        public void WriteProperties()
+        {
+            Console.WriteLine("width: {0}", width);
+            Console.WriteLine("height: {0}", height);
+            Console.WriteLine("color: {0}", color);
+            Console.WriteLine("is Transparent: {0} \n", isTransparent);
+        }
+    }
+}
